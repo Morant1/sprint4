e0d8fbb9a90cd97aaeaf498bbc8692015c3368ca
litalg8 <litalgottlieb@gmail.com>
2020-09-16 18:43:21 +0300

    img preview

--- frontend/src/cmps/EventiPreview.jsx
+++ frontend/src/cmps/EventiPreview.jsx
@@ -9,7 +9,7 @@ export function EventiPreview({ eventi }) {
     return (
         <div className="eventi-preview card">
             <button className="attend-btn">40/100 going{eventi.members}</button>
-            <img className="card-img">{eventi.imgUrls}</img>
+            <img className="card-img" src={eventi.imgUrls}/>
             <div className="card-title">{eventi.title}</div>
             <div className="card-date">{eventi.startsAt}</div>
             <Link to={`/event/${eventi._id}`}></Link>
