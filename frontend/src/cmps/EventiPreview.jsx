1491768f572e39e580ee593602a988f68cb22727
litalg8 <litalgottlieb@gmail.com>
2020-09-17 22:48:41 +0300

    more details

--- frontend/src/cmps/EventiPreview.jsx
+++ frontend/src/cmps/EventiPreview.jsx
@@ -5,15 +5,17 @@ import { Link } from 'react-router-dom'
 
 // redirect to details 'category:_id'
 export function EventiPreview({ eventi, currTag }) {
-console.log(eventi._id);
-const img = eventi.ImgUrl ? require(`../assets/img/${eventi._id}.jpg`) : require(`../assets/img/default.jpg`)
+    console.log(eventi._id);
+    const img = eventi.ImgUrl ? require(`../assets/img/${eventi._id}.jpg`) : require(`../assets/img/default.jpg`)
     return (
         <div className="eventi-preview card">
-            <img className="card-img" alt="event-01" src={img}/>
-            <button className="attend-btn">40/100 going{eventi.members}</button>
-            <div className="card-title">{eventi.title}</div>
-            <div className="card-date">{eventi.duration}hours</div>
-            <Link to={`/${currTag}/${eventi._id}`}>More Details</Link>
+            <img className="preview-img" alt="event-01" src={img} />
+            <div className="preview-info">
+                <button className="attend-btn">40/100 going{eventi.members}</button>
+                <div className="preview-title">{eventi.title}</div>
+                <div className="preview-date">{eventi.duration}hours</div>
+                <Link to={`/${currTag}/${eventi._id}`}>More Details</Link>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/EventiDetails.jsx
+++ frontend/src/pages/EventiDetails.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { eventService } from '../services/eventService';
 import { Link } from 'react-router-dom'
-
-// import { loadEvents } from '../store/actions/eventActions';
+import AccessTime from '@material-ui/icons/AccessTime'
+import StarIcon from '@material-ui/icons/Star'
 // import { connect } from 'react-redux';
+
+import { eventService } from '../services/eventService';
+// import { loadEvents } from '../store/actions/eventActions';
 //Redirect to Edit event
 // Map // Review // Attanding //Chat
 
@@ -17,49 +19,81 @@ export class EventiDetails extends Component {
 
   loadEventi = () => {
     const { _id } = this.props.match.params
-    console.log(_id);
+    // console.log(_id)
     eventService.getById(_id)
       .then(eventi => {
-        this.setState({ eventi }, () => console.log(eventi))
+        this.setState({ eventi })
       })
 
   }
 
   render() {
     const { eventi } = this.state
+    const { events } = this.props
     if (!eventi) return <div>Loading...</div>
     return (
-      <section className="card-details">
+      <section className="eventi-details">
         <div className="eventi-photo-grid">
           <img src={require('../assets/img/game-1.jpg')} alt="event-1" />
           <img src={require('../assets/img/game-2.jpg')} alt="event-2" />
           <img src={require('../assets/img/game-3.jpg')} alt="event-3" />
         </div>
-        <Link to={`/event/edit/${eventi._id}`}>Edit Event</Link>
-        <div className="eventi-title">
+        <div className="eventi-title flex">
           <h2>{eventi.title}</h2>
-          {/* <h5>{eventi.rate}</h5>
-          <h5>{eventi.location}</h5>
-          <h5>{eventi.creator}</h5> */}
+          <Link to={`/event/edit/${eventi._id}`}>Edit Event</Link>
         </div>
-       <h3>What it's all About:</h3>
-        <p>{eventi.description}</p>
+        <div className="eventi-subtitle flex">
+          <StarIcon />
+          <h5>{eventi.rank}(3147)</h5>
+          <h5>| Paris, France</h5>
+          <h5>| Hosted By:{eventi.createdBy.fullName}</h5>
+        </div>
         <div className="eventi-info">
-        <h3>{eventi.duration} hours</h3>
+          <h3><AccessTime />{eventi.duration} hours</h3>
           <h4>Join with your computer, phone or tablet</h4>
           <h4>Hosted in English</h4>
           {/* <h3>{eventi.participants}</h3> */}
-   
         </div>
-      </section >
+        <div className="eventi-description">
+          <h3>What it's all About:</h3>
+          <p>{eventi.description}</p>
+        </div>
+        <div className="eventi-participants">
+          <h3>Who's coming?</h3>
+          <div>
+            {
+              eventi.participants.map(participant => {
+                return <div key={participant._id}>
+                  {participant.fullName}
+                </div>
+              })
+            }
+          </div>
+
+        </div>
+
+        <div className="eventi-comments">
+          <h3>Comment and Reviews</h3>
+          <div>
+            {
+              eventi.comments.map(comment => {
+                return <div key={comment._id}>
+                  {comment.txt}
+                </div>
+              })
+            }
+          </div>
+
+        </div>
+      </section>
 
 
 
 
 
- 
 
-      
+
+
 
 
     )
