c58e2e27cb38d490949bf197790b2c51da176bb4
Morant1 <moran8865@walla.com>
2020-09-18 15:58:44 +0300

    grid

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,12 +11,19 @@ export class HomePage extends Component {
   render() {
     return (
       <section className="events-grid">
-          <div className="tag tag-1" onClick={(ev)=>{this.redirectClick('Football')}}>
-              Football
+        <div className="tag tag-1" onClick={(ev) => { this.redirectClick('Football') }}>
+        <div className="tag-title">Football</div>
             </div>
-          <div className="tag tag-2" onClick={(ev)=>{this.redirectClick('LifeStyle')}}>LifeStyle</div>
-          <div className="tag tag-3" onClick={(ev)=>{this.redirectClick('Sport')}}>Sport</div>
-          <div className="tag tag-3" onClick={(ev)=>{this.redirectClick('Music')}}>Music</div>
+        <div className="tag tag-2"
+          onClick={(ev) => { this.redirectClick('LifeStyle') }
+          }>
+          <div className="tag-title">LifeStyle</div>
+        </div>
+        <div className="tag tag-3" onClick={(ev) => { this.redirectClick('Sport') }}>
+          <div className="tag-title">Sport</div></div>
+        <div className="tag tag-4" onClick={(ev) => { this.redirectClick('Music') }}>
+          <div className="tag-title">Music</div>
+        </div>
       </section>
     );
   }
