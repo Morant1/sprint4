30246b5512a6da533c7c55dcc1b5ee74af3933d8
litalg8 <litalgottlieb@gmail.com>
2020-09-22 13:24:22 +0300

    preview and details

--- frontend/src/cmps/EventiList.jsx
+++ frontend/src/cmps/EventiList.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { EventiPreview } from './EventiPreview'
 import { EventiFilter } from './EventiFilter'
 
-export function EventiList({ events ,onSetFilter , currTag}) {
+export function EventiList({ events ,onSetFilter , currTag, onAddParticipant}) {
     return (
         <React.Fragment>
             <EventiFilter onSetFilter={onSetFilter}/>
             {
-                events.map(eventi => <EventiPreview eventi={eventi} key={eventi._id} currTag={currTag} />)
+                events.map(eventi => <EventiPreview eventi={eventi} key={eventi._id} currTag={currTag} onAddParticipant={onAddParticipant} />)
             }
         </React.Fragment>
     )
