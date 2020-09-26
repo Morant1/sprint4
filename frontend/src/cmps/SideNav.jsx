8e9fa686e65f606fdc645e500fcc6a7bf8b49b49
Morant1 <moran8865@walla.com>
2020-09-26 20:41:23 +0300

    filter

--- frontend/src/cmps/SideNav.jsx
+++ frontend/src/cmps/SideNav.jsx
@@ -4,6 +4,13 @@ import { withRouter } from "react-router";
 
 
 export class _SideNav extends Component {
+
+    componentDidMount() {
+        setTimeout(() => {
+            document.querySelector('.side-nav').classList.add('trans');
+            
+        }, 200);
+    }
     redirectClick = (tag) => {
         this.props.history.push(`/${tag}`)
     }
@@ -14,13 +21,15 @@ export class _SideNav extends Component {
 
             <section className="side-nav flex">
                 <div className="tag-list">
-                    <div className="tag-item" onClick={(ev) => { this.redirectClick('All') }}>All</div>
                     <div className="tag-item" onClick={(ev) => { this.redirectClick('Sport') }}>Sport</div>
                     <div className="tag-item" onClick={(ev) => { this.redirectClick('Movies') }}>Movies</div>
                     <div className="tag-item" onClick={(ev) => { this.redirectClick('TVShows') }}>TV shows</div>
                     <div className="tag-item" onClick={(ev) => { this.redirectClick('Books') }}>Books</div>
                     <div className="tag-item" onClick={(ev) => { this.redirectClick('Art') }}>Art</div>
                     <div className="tag-item" onClick={(ev) => { this.redirectClick('Comics') }}>Comics</div>
+                    <div className="all-tag">
+                    <div className="tag-item" onClick={(ev) => { this.redirectClick('All') }}>All</div>
+                    </div>
                 </div>
                 <EventiFilter onSetFilter={onSetFilter} />
             </section>
